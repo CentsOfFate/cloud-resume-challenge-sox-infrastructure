1a44d1f56a8daa1a31362e488136ef743f277bed
CentsOfFate <charlie.dalldorf@gmail.com>
2022-10-28 04:22:09 -0500

    #1 - Set Default Variables for Terraform Cloud Environment.

--- variables.tf
+++ variables.tf
@@ -11,7 +11,13 @@ variable "tenant_id" {
 }
 
 variable "resource_group" {
+  type = string
+  default = "CRCSOX-Prod"
+  description = "Azure Resource Groups for Cloud Resume Challenge - SOX Edition"
 }
 
 variable "static_site_name" {
+  type = string
+  default = "CRCSOX-Client-Prod"
+  description = "Static Site Name for Resume Client"
 }
