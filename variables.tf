c5f74393d9ffd32f07fb81d17fb0473530aa384f
CentsOfFate <charlie.dalldorf@gmail.com>
2022-10-28 04:44:16 -0500

    #1 - Retrying Applying Changes to Azure.

--- variables.tf
+++ variables.tf
@@ -11,13 +11,13 @@ variable "tenant_id" {
 }
 
 variable "resource_group" {
-  type = string
-  default = "CRCSOX-Prod"
+  type        = string
+  default     = "CRCSOX-Prod"
   description = "Azure Resource Groups for Cloud Resume Challenge - SOX Edition"
 }
 
 variable "static_site_name" {
-  type = string
-  default = "CRCSOX-Client-Prod"
+  type        = string
+  default     = "CRCSOX-Client-Prod"
   description = "Static Site Name for Resume Client"
 }
