7a2e4729ff0e6ad9288ce573613d86e4fc7fef37
CentsOfFate <charlie.dalldorf@gmail.com>
2022-11-01 09:43:14 -0500

    Changes.

--- azure_storage_account.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_storage_account" "storage" {
-  name                     = "crcsox"
-  resource_group_name      = "CRCSOX-Dev"
-  location                 = "Central US"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
