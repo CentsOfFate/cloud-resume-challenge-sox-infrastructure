8375a2c6752dd46b1e1766359005fba5560f06a9
CentsOfFate <charlie.dalldorf@gmail.com>
2022-11-01 09:24:20 -0500

    #1 - No Storage name or Key just Functions App

--- azure_functions.tf
+++ azure_functions.tf
@@ -3,8 +3,6 @@ resource "azurerm_windows_function_app" "crcsox" {
   resource_group_name = "CRCSOX-Dev"
   location            = "Central US"
 
-  storage_account_name       = azurerm_storage_account.storage.name
-  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
   service_plan_id            = azurerm_service_plan.service.id
 
   site_config {}
