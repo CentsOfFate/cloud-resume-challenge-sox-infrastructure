663fa1df3dcada8022113e74417f43226b98a0c0
CentsOfFate <charlie.dalldorf@gmail.com>
2022-11-01 09:32:38 -0500

    #1 - Changes.

--- azure_functions.tf
+++ azure_functions.tf
@@ -2,7 +2,10 @@ resource "azurerm_windows_function_app" "crcsox" {
   name                = var.functions_name
   resource_group_name = "CRCSOX-Dev"
   location            = "Central US"
-  service_plan_id     = azurerm_service_plan.service.id
+
+  storage_account_name       = "crcsox"
+  storage_account_access_key = "+/KCi7ZiF1cDQUghZTJ7Aw/vILhOa+Opu4cLItaCbJ7UsIYHpLRcsfILI4ZbLhR1ArDfD2GG5htZ+AStC6Gbxg=="
+  service_plan_id            = azurerm_service_plan.service.id
 
   site_config {}
 }
