35abcc37f0b055df8edaaeb4aa9aec3bdd72afd5
CentsOfFate <charlie.dalldorf@gmail.com>
2022-11-01 08:49:58 -0500

    #1 - Change to Standard

--- azure_storage_account.tf
+++ azure_storage_account.tf
@@ -2,6 +2,6 @@ resource "azurerm_storage_account" "example" {
   name                     = "crcsox"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = "Central US"
-  account_tier             = "StorageV2"
+  account_tier             = "Standard"
   account_replication_type = "LRS"
 }
