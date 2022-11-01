cb617d5e8fea2b1f6d526b084b10429a49010ee9
CentsOfFate <charlie.dalldorf@gmail.com>
2022-11-01 09:04:57 -0500

    #1 - Provision Service Plan

--- azure_service_plan.tf
+++ azure_service_plan.tf
@@ -1,6 +1,6 @@
 resource "azurerm_service_plan" "service" {
   name                = "CRCSOX-Service-Plan"
-  resource_group_name = azurerm_resource_group.rg
+  resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   os_type             = "Windows"
   sku_name            = "Y1"
