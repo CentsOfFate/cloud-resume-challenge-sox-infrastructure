5e0bd2aa6bc3ff8295aee88c460f19bf767c2ebb
CentsOfFate <charlie.dalldorf@gmail.com>
2022-11-01 06:32:35 -0500

    Run using OIDC Login.

--- providers.tf
+++ providers.tf
@@ -1,8 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.27.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "CRCSOX-Dev"
+    storage_account_name = "CRCSOX"
+    container_name       = "tfstate"
+    key                  = "dev.teroidc.tfstate"
+  }
+}
+
 provider "azurerm" {
   features {}
-
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
 }
