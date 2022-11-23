568043549d88cb61d61657e38f67adfe5774a30c
CentsOfFate <charlie.dalldorf@gmail.com>
2022-11-23 12:44:54 -0600

    Empty Variables.

--- variables.tf
+++ variables.tf
@@ -1,16 +1,21 @@
 variable "client_id" {
+  type = string
+  default = ""
 }
 
 variable "client_secret" {
-
+  type = string
+  default = ""
 }
 
 variable "subscription_id" {
-
+  type = string
+  default = ""
 }
 
 variable "tenant_id" {
-
+  type = string
+  default = ""
 }
 
 variable "static_site_name" {
