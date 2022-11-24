c565d49740a7d7d0d26fffb58634a37fb852fbdf
Charlie Dalldorf <33187112+charliedalldorf@users.noreply.github.com>
2022-11-23 20:56:11 -0600

    Empty Variables. (#67)
    Use Azure OIDC + Terraform Cloud

--- variables.tf
+++ variables.tf
@@ -1,18 +1,3 @@
-variable "client_id" {
-}
-
-variable "client_secret" {
-
-}
-
-variable "subscription_id" {
-
-}
-
-variable "tenant_id" {
-
-}
-
 variable "static_site_name" {
   type        = string
   default     = "CRCSOX-Client-Dev"
