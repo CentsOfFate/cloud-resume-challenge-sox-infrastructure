aef5654704cdc2004252857d9b3c1510f7326553
CentsOfFate <charlie.dalldorf@gmail.com>
2022-11-23 20:02:52 -0600

    chghfhsd

--- variables.tf
+++ variables.tf
@@ -1,23 +1,3 @@
-variable "client_id" {
-  type  = string
-  default = ""
-}
-
-variable "client_secret" {
-  type    = string
-  default = ""
-}
-
-variable "subscription_id" {
-  type    = string
-  default = ""
-}
-
-variable "tenant_id" {
-  type    = string
-  default = ""
-}
-
 variable "static_site_name" {
   type        = string
   default     = "CRCSOX-Client-Dev"
